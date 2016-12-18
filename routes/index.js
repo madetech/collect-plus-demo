3a7e1cbf9c851a7811c33fcbbadd43a32112ad20
Jakub Naliwajek <jakub.naliwajek@airhelp.com>
2016-12-18 12:59:48 +0100

    Keep API keys in a .env file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+    google_maps_api_key: process.env.GOOGLE_MAPS_API_KEY
+  });
 });
 
 module.exports = router;
