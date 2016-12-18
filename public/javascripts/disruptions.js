3adbff7c8ddae35d6f40d7334533271d766e426d
Jakub Naliwajek <jakub.naliwajek@airhelp.com>
2016-12-18 13:18:50 +0100

    Add popup description for test marker

--- public/javascripts/disruptions.js
+++ public/javascripts/disruptions.js
@@ -8,9 +8,17 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     // test marker
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
         position: new google.maps.LatLng(51.539282, -0.36197),
         map: map,
         title: 'Traffic is slow on the approach with tailbacks reaching beyond Polish War Memorial. Expect delays.'
+    });
+
+    marker.addListener('click', function() {
+        var infoWindow = new google.maps.InfoWindow({
+            content: 'Traffic is slow on the approach with tailbacks reaching beyond Polish War Memorial. Expect delays.'
+        });
+
+        infoWindow.open(map, marker);
     });
 }
